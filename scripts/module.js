9df3c4ee82b38b2aff49a349a0df0d2f3278be7f
Felipe Dias <philixo@gmail.com>
2020-12-19 15:24:12 -0600

    debug

--- scripts/module.js
+++ scripts/module.js
@@ -10,3 +10,28 @@ Hooks.once('ready', async function() {
 
 
 console.log('!!!!!!!!!!!!TESTING THREE!!!!!!!!!!!!');
+
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+x = game.scenes.values()
+result = x.next();
+while (!result.done) {
+    result.value.activate()
+    console.log('!!!!!!!!!!!! ACTIVATING!!!!!!!!!!!!!!!!!!!!!!'); 
+    console.log(result.value.data.name); 
+    sleep(3000);
+ result = x.next();
+}
+
+// get directory-item entity from "HTML Elements"
+// find its ID
+this_id = 'Qgs0iTBcKXmQM1lI';
+
+for (this_scene in game.scenes.entities){
+    if (this_scene.id == this_id){
+        this_scene.view();
+    };
+}
