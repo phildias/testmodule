aca9edd39209ae132e297fc7ff4207571ff547c2
Felipe Dias <philixo@gmail.com>
2020-12-19 15:24:49 -0600

    Update module.js

--- scripts/module.js
+++ scripts/module.js
@@ -28,10 +28,21 @@ while (!result.done) {
 
 // get directory-item entity from "HTML Elements"
 // find its ID
+// compare with all ids in game.scenes
 this_id = 'Qgs0iTBcKXmQM1lI';
 
-for (this_scene in game.scenes.entities){
-    if (this_scene.id == this_id){
-        this_scene.view();
-    };
+for (const this_scene in game.scenes.entities){
+    console.log('FUCK');
+    console.log('${this_scene.id}');
+
+    //if (this_scene.id == this_id){
+    //    this_scene.view();
+    //};
+}
+
+
+var obj = {a: 1, b: 2, c: 3};
+
+for (const prop in obj) {
+  console.log(`obj.${prop} = ${obj[prop]}`);
 }
